301289921fabffaf51fe155b5485f75bba1c41e4
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-03-10 21:07:53 +0100

    Rebuild

--- build/restyle.node.js
+++ build/restyle.node.js
@@ -36,50 +36,16 @@ module.exports = (function (O) {
     empty = [],
     restyle;
 
-  function ReStyle(component, css, prefixes, doc) {
-    this.component = component;
-    this.css = css;
-    this.prefixes = prefixes;
-    this.doc = doc;
-  }
-
-  function replace(substitute) {
-    if (!(substitute instanceof ReStyle)) {
-      substitute = restyle(
-        this.component, substitute, this.prefixes, this.doc
-      );
-    }
-    ReStyle.call(
-      this,
-      substitute.component,
-      substitute.css,
-      substitute.prefixes,
-      substitute.doc
-    );
-  }
-
-  ReStyle.prototype = {
-    overwrite: replace,
-    replace: replace,
-    set: replace,
-    valueOf: function () {
-      return this.css;
-    }
-  };
-
   function camelReplace(m, $1, $2) {
     return $1 + '-' + $2.toLowerCase();
   }
 
-  function create(key, value, prefixes) {
+  function create(key, value) {
     var
       css = [],
       pixels = typeof value === 'number' ? 'px' : '',
       k = key.replace(camelFind, camelReplace),
       i;
-    for (i = 0; i < prefixes.length; i++) {
-      css.push('-', prefixes[i], '-', k, ':', value, pixels, ';');
-    }
     css.push(k, ':', value, pixels, ';');
     return css.join('');
   }
@@ -88,7 +54,7 @@ module.exports = (function (O) {
     return previous.length ? previous + '-' + key : key;
   }
 
-  function generate(css, previous, obj, prefixes) {
+  function generate(css, previous, obj) {
     var key, value, i;
     for (key in obj) {
       if (has.call(obj, key)) {
@@ -97,20 +63,19 @@ module.exports = (function (O) {
             value = obj[key];
             for (i = 0; i < value.length; i++) {
               css.push(
-                create(property(previous, key), value[i], prefixes)
+                create(property(previous, key), value[i])
               );
             }
           } else {
             generate(
               css,
               property(previous, key),
-              obj[key],
-              prefixes
+              obj[key]
             );
           }
         } else {
           css.push(
-            create(property(previous, key), obj[key], prefixes)
+            create(property(previous, key), obj[key])
           );
         }
       }
@@ -118,9 +83,10 @@ module.exports = (function (O) {
     return css.join('');
   }
 
-  function parse(component, obj, prefixes) {
+  function parse(obj) {
     var
       css = [],
+      component = '',
       at, cmp, special, k, v,
       same, key, value, i, j;
     for (key in obj) {
@@ -136,17 +102,11 @@ module.exports = (function (O) {
         for (i = 0; i < value.length; i++) {
           v = value[i];
           if (special) {
-            j = prefixes.length;
-            while (j--) {
-              css.push('@-', prefixes[j], '-', k, '{',
-                parse(cmp, v, [prefixes[j]]),
-                '}');
-            }
-            css.push(key, '{', parse(cmp, v, prefixes), '}');
+            css.push(key, '{', parse(cmp, v), '}');
           } else {
             css.push(
               same ? key : component + key,
-              '{', generate([], '', v, prefixes), '}'
+              '{', generate([], '', v), '}'
             );
           }
         }
@@ -155,309 +115,8 @@ module.exports = (function (O) {
     return css.join('');
   }
 
-  // hack to avoid JSLint shenanigans
-  if ({undefined: true}[typeof document]) {
-    // in node, by default, no prefixes are used
-    restyle = function (component, obj, prefixes) {
-      if (typeof component === 'object') {
-        prefixes = obj;
-        obj = component;
-        component = '';
-      } else {
-        component += ' ';
-      }
-      return parse(component, obj, prefixes || empty);
-    };
-    // useful for different style of require
-    restyle.restyle = restyle;
-  } else {
-    restyle = function (component, obj, prefixes, doc) {
-      if (typeof component === 'object') {
-        doc = prefixes;
-        prefixes = obj;
-        obj = component;
-        c = (component = '');
-      } else {
-        c = component + ' ';
-      }
-      var c, d = doc || (doc = document),
-        css = parse(c, obj, prefixes || (prefixes = restyle.prefixes));
-      return new ReStyle(component, css, prefixes, doc);
-    };
-  }
-
-  // bringing animation utility in window-aware world only
-  if (!{undefined: true}[typeof window]) {
-    restyle.animate = (function (g) {
-
-      var
-        rAF = window.requestAnimationFrame ||
-              window.webkitRequestAnimationFrame ||
-              window.mozRequestAnimationFrame ||
-              window.msRequestAnimationFrame ||
-              function (fn) { setTimeout(fn, 10); },
-        liveStyles = {},
-        uid = 'restyle-'.concat(Math.random() * (+new Date()), '-'),
-        uidIndex = 0,
-        animationType,
-        transitionType
-      ;
-
-      switch (true) {
-        case !!g.AnimationEvent:
-          animationType = 'animationend';
-          break;
-        case !!g.WebKitAnimationEvent:
-          animationType = 'webkitAnimationEnd';
-          break;
-        case !!g.MSAnimationEvent:
-          animationType = 'MSAnimationEnd';
-          break;
-        case !!g.OAnimationEvent:
-          animationType = 'oanimationend';
-          break;
-      }
-
-      switch (true) {
-        case !!g.TransitionEvent:
-          transitionType = 'transitionend';
-          break;
-        case !!g.WebKitTransitionEvent:
-          transitionType = 'webkitTransitionEnd';
-          break;
-        case !!g.MSTransitionEvent:
-          transitionType = 'MSTransitionEnd';
-          break;
-        case !!g.OTransitionEvent:
-          transitionType = 'oTransitionEnd';
-          break;
-      }
-
-      restyle.transition = function (el, info, callback) {
-        var
-          transition = info.transition || 'all .3s ease-out',
-          id = el.getAttribute('id'),
-          to = [].concat(info.to),
-          from = update({}, info.from),
-          noID = !id,
-          style = {},
-          currentID,
-          result,
-          live,
-          t
-        ;
-        function drop() {
-          if (transitionType) {
-            el.removeEventListener(transitionType, onTransitionEnd, false);
-          } else {
-            clearTimeout(t);
-            t = 0;
-          }
-        }
-        function next() {
-          style[currentID] = (live.last = update(from, to.shift()));
-          live.css.replace(style);
-          if (transitionType) {
-            el.addEventListener(transitionType, onTransitionEnd, false);
-          } else {
-            t = setTimeout(onTransitionEnd, 10);
-          }
-        }
-        function onTransitionEnd(e) {
-          drop();
-          if (to.length) {
-            rAF(next);
-          } else {
-            if (!e) e = new CustomEvent('transitionend', {detail: result});
-            else e.detail = result;
-            if (callback) callback.call(el, e);
-          }
-        }
-        function update(target, source) {
-          for (var k in source) target[k] = source[k];
-          return target;
-        }
-        if (noID) el.setAttribute('id', id = (uid + uidIndex++).replace('.','-'));
-        currentID = '#' + id;
-        if (liveStyles.hasOwnProperty(id)) {
-          live = liveStyles[id];
-          from = (live.last = update(live.last, from));
-          style[currentID] = from;
-          live.transition.remove();
-          live.css.replace(style);
-        } else {
-          live = liveStyles[id] = {
-            last: (style[currentID] = from),
-            css: restyle(style)
-          };
-        }
-        rAF(function() {
-          style[currentID] = {transition: transition};
-          live.transition = restyle(style);
-          rAF(next);
-        });
-        return (result = {
-          clean: function () {
-            if (noID) el.removeAttribute('id');
-            drop();
-            live.transition.remove();
-            live.css.remove();
-            delete liveStyles[id];
-          },
-          drop: drop
-        });
-      };
-
-      ReStyle.prototype.getAnimationDuration = function (el, name) {
-        for (var
-          chunk, duration,
-          classes = el.className.split(/\s+/),
-          i = classes.length; i--;
-        ) {
-          chunk = classes[i];
-          if (
-            chunk.length &&
-            (new RegExp('\\.' + chunk + '(?:|\\{|\\,)([^}]+?)\\}')).test(this.css)
-          ) {
-            chunk = RegExp.$1;
-            if (
-              (new RegExp(
-                'animation-name:' +
-                name +
-                ';.*?animation-duration:([^;]+?);'
-              )).test(chunk) ||
-              (new RegExp(
-                'animation:\\s*' + name + '\\s+([^\\s]+?);'
-              )).test(chunk)
-            ) {
-              chunk = RegExp.$1;
-              duration = parseFloat(chunk);
-              if (duration) {
-                return duration * (/[^m]s$/.test(chunk) ? 1000 : 1);
-              }
-            }
-          }
-        }
-        return -1;
-      };
-
-      ReStyle.prototype.getTransitionDuration = function (el) {
-        var
-          cs = getComputedStyle(el),
-          duration = cs.getPropertyValue('transition-duration') ||
-                     /\s(\d+(?:ms|s))/.test(
-                       cs.getPropertyValue('transition')
-                     ) && RegExp.$1
-        ;
-        return parseFloat(duration) * (/[^m]s$/.test(duration) ? 1000 : 1);
-      };
-
-      ReStyle.prototype.transit = transitionType ?
-        function (el, callback) {
-          function onTransitionEnd(e) {
-            drop();
-            callback.call(el, e);
-          }
-          function drop() {
-            el.removeEventListener(transitionType, onTransitionEnd, false);
-          }
-          el.addEventListener(transitionType, onTransitionEnd, false);
-          return {drop: drop};
-        } :
-        function (el, callback) {
-          var i = setTimeout(callback, this.getTransitionDuration(el));
-          return {drop: function () {
-            clearTimeout(i);
-          }};
-        }
-      ;
-
-      ReStyle.prototype.animate = animationType ?
-        function animate(el, name, callback) {
-          function onAnimationEnd(e) {
-            if (e.animationName === name) {
-              drop();
-              callback.call(el, e);
-            }
-          }
-          function drop() {
-            el.removeEventListener(animationType, onAnimationEnd, false);
-          }
-          el.addEventListener(animationType, onAnimationEnd, false);
-          return {drop: drop};
-        } :
-        function animate(el, name, callback) {
-          var i, drop, duration = this.getAnimationDuration(el, name);
-          if (duration < 0) {
-            drop = O;
-          } else {
-            i = setTimeout(
-              function () {
-                callback.call(el, {
-                  type: 'animationend',
-                  animationName: name,
-                  currentTarget: el,
-                  target: el,
-                  stopImmediatePropagation: O,
-                  stopPropagation: O,
-                  preventDefault: O
-                });
-              },
-              duration
-            );
-            drop = function () {
-              clearTimeout(i);
-            };
-          }
-          return {drop: drop};
-        }
-      ;
-    }(window));
-  }
-
-  restyle.customElement = function (name, constructor, proto) {
-    var
-      key,
-      ext = 'extends',
-      prototype = Object.create(constructor.prototype),
-      descriptor = {prototype: prototype},
-      has = descriptor.hasOwnProperty,
-      isExtending = proto && has.call(proto, ext)
-    ;
-    if (isExtending) {
-      descriptor[ext] = proto[ext];
-    }
-    for (key in proto) {
-      if (key !== ext) {
-        prototype[key] = (
-          key === 'css' ?
-            restyle(
-              isExtending ?
-               (proto[ext] + '[is=' + name + ']') :
-               name,
-              proto[key]
-            ) :
-            proto[key]
-        );
-      }
-    }
-    return document.registerElement(name, descriptor);
+  return function restyle (obj) {
+    return parse(obj);
   };
-
-  restyle.prefixes = [
-    'webkit',
-    'moz',
-    'ms',
-    'o'
-  ];
-
-  return restyle;
-
-/**
- * not sure if TODO since this might be prependend regardless the parser
- *  @namespace url(http://www.w3.org/1999/xhtml);
- *  @charset "UTF-8";
- */
-
 }({}))
 ;
